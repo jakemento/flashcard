ded2bd1fd1e75bf6b1fac042faaf5706afd9a4f3
jws <jws@midiman.local>
2016-01-11 17:43:11 -0800

    add more definitions to github for the flashcard site

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,9 +5,46 @@ $(function() {
   });
 });
 
+
 $(function() {
   $(".clickable2").click(function() {
     $("initially-showing2").toggle();
     $("#initially-hidden2").toggle();
+  });
+});
+
+
+$(function() {
+  $(".clickable3").click(function() {
+    $("initially-showing3").toggle();
+    $("#initially-hidden3").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable4").click(function() {
+    $("initially-showing4").toggle();
+    $("#initially-hidden4").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable5").click(function() {
+    $("initially-showing5").toggle();
+    $("#initially-hidden5").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable6").click(function() {
+    $("initially-showing6").toggle();
+    $("#initially-hidden6").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable7").click(function() {
+    $("initially-showing7").toggle();
+    $("#initially-hidden7").toggle();
   });
 });
